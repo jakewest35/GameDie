6e0e2f868697f9e3eddbf1036529e19c723ed75b
Jake West <jswest95@yahoo.com>
2023-02-20 11:49:48 -0800

    Resolve #106, add percentages

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,46 +1,48 @@
 #include "GameDie.h"
-#include <vector>
+
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
-//class constructor that seeds the random number generator
-GameDie::GameDie()
-{
-    srand(time(NULL));
+// class constructor that seeds the random number generator
+GameDie::GameDie() {
+  srand(time(NULL));
+  counter.resize(FACES);
+
+  for (int i = 0; i < FACES; i++) counter[i] = 0;
+}
+
+// overloaded constructor
+GameDie::GameDie(unsigned int num) {
+  if (num == 0) {
     counter.resize(FACES);
-
-    for(int i=0; i<FACES; i++)
-      counter[i] = 0;
+  } else {
+    counter.resize(num);
+  }
+  for (int i = 0; i < FACES; i++) {
+    counter[i] = 0;
+  }
 }
 
-//overloaded constructor
-GameDie::GameDie(unsigned int num)
-{
-    if( num == 0 )
-    {
-        counter.resize(FACES);
-    }
-    else{
-        counter.resize(num);
-    }
-    for(int i=0; i<FACES; i++)
-    {
-        counter[i] = 0;
-    }
-
-}
-
-//generate a random number between 1-n where n is the counter size
-// (inclusive) and return it
-int GameDie::roll()
-{
-    int roll = rand() % counter.size();
-    counter[roll]++;
-    return roll + 1;
+// generate a random number between 1-n where n is the counter size
+//  (inclusive) and return it
+int GameDie::roll() {
+  int roll = rand() % counter.size();
+  counter[roll]++;
+  return roll + 1;
 }
 
 // return the count of how many times each face has been rolled, as a vector
 // where each face's count is at index face-1 (i.e. Face 1 is at index 0)
-vector <int> GameDie::get_distribution(){
-    return counter;
+vector<int> GameDie::get_distribution() { return counter; }
+
+// returns percent of rolling each side of the die
+vector<double> GameDie::get_percentages() {
+  if (counter[0] == 0) {
+    for (int i = 0; i < (int)FACES; i++) percentages.push_back(0);
+    return percentages;
+  }
+  double pct = (double)(1 / FACES);
+  for (int i = 0; i < (int)FACES; i++) percentages.push_back(pct);
+  return percentages;
 }
--- GameDie.h
+++ GameDie.h
@@ -4,17 +4,18 @@
 
 using std::vector;
 
-class GameDie
-{
-  public:
-   GameDie();
-   GameDie(unsigned int);
-   int roll();
-   vector <int> get_distribution();
+class GameDie {
+ public:
+  GameDie();
+  GameDie(unsigned int);
+  int roll();
+  vector<int> get_distribution();
+  vector<double> get_percentages();
 
-  private:
-   vector <int> counter;
-   const static int FACES = 6;
+ private:
+  vector<int> counter;
+  vector<double> percentages;
+  const static int FACES = 6;
 };
 
 #endif
